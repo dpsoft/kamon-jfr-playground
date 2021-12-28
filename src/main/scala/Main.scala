199a929197f1d466bfb85cd14fa1d7fe5e12293c
Diego <diegolparra@gmail.com>
2021-12-27 23:10:09 -0300

    minor changes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 import com.typesafe.config.Config
 import jdk.jfr.Configuration
-import jdk.jfr.consumer.{EventStream, RecordedEvent, RecordingStream}
+import jdk.jfr.consumer.{EventStream, RecordedEvent, RecordingStream, MetadataEvent}
 import kamon.Kamon
 import kamon.metric.PeriodSnapshot
 import kamon.module.MetricReporter
@@ -10,92 +10,82 @@ import java.util.function.Consumer
 import scala.collection.concurrent.TrieMap
 import scala.util.Using
 import java.time.Instant
+import scala.jdk.CollectionConverters._
 
+
+object KamonJfr {
+  @volatile var jfrConfig = jfrConfigFrom(Kamon.config());
+
+  def jfrConfigFrom(config:Config): Map[String, String] = {
+    config
+    .getConfig("kamon.jfr")
+    .getStringList("config")
+    .asScala.map(_.split("="))
+    .map(a => a(0) -> a(1))
+    .toMap
+  }
+}
 
 @main def hello: Unit = {
 
  val safepointBegin = TrieMap.empty[Long, Instant]
 
- Kamon.counter("Pepe").withoutTags().increment()
+//  Kamon.init()
 
- Kamon.init()
- Kamon.registerModule("Metrics", new MetricReporter {
-   override def reportPeriodSnapshot(snapshot: PeriodSnapshot): Unit = {
-     println(snapshot)
-   }
+//  Kamon.registerModule("Metrics", new MetricReporter {
+//    override def reportPeriodSnapshot(snapshot: PeriodSnapshot): Unit = {
+//      println(snapshot)
+//    }
 
-   override def stop(): Unit = {}
+//    override def stop(): Unit = {}
 
-   override def reconfigure(newConfig: Config): Unit = {}
- })
+//    override def reconfigure(newConfig: Config): Unit = {}
+//  })
  
- System.gc()
+//  System.gc()
 
- val configuration = Configuration.getConfiguration("default")
- println(configuration.getSettings())
+//  val configuration = Configuration.getConfiguration("default")
+//  println(configuration.getSettings())
+ var configuration = KamonJfr.jfrConfig
 
- Using.resource(RecordingStream(configuration)) { es =>
-//
+ Using.resource(RecordingStream()) { es =>  
+   es.setSettings(configuration.asJava)
+   es.onMetadata(metadata => onMetadata(metadata))
+   es.onEvent(e => onEvent(e))
+   // regixster event handlersxxxxxx
+  //  es.onEvent("jdk.CPULoad", onCPULoad)
+  //  es.onEvent("jdk.JavaThreadStatistics", onJavaThreadStatistics)
+  //  es.onEvent("jdk.SafepointBegin", onSafepointBegin)
+  //  es.onEvent("jdk.SafepointEnd", onSafepointEnd)
 
-   val duration = Duration.ofSeconds(1)
-   es.enable("jdk.CPULoad").withPeriod(duration)
-   es.enable("jdk.YoungGarbageCollection").withoutThreshold
-   es.enable("jdk.OldGarbageCollection").withoutThreshold
-   es.enable("jdk.GCHeapSummary").withPeriod(duration)
-   es.enable("jdk.PhysicalMemory").withPeriod(duration)
-   es.enable("jdk.GCConfiguration").withPeriod(duration)
-   es.enable("jdk.SafepointBegin")
-   es.enable("jdk.SafepointEnd")
-   es.enable("jdk.ObjectAllocationSample").`with`("throttle", "150/s")
-   es.enable("jdk.ExecutionSample").withPeriod(Duration.ofMillis(10)).withStackTrace
-   es.enable("jdk.JavaThreadStatistics").withPeriod(duration)
-   es.enable("jdk.ClassLoadingStatistics").withPeriod(duration)
-   es.enable("jdk.Compilation").withoutThreshold
-   es.enable("jdk.GCHeapConfiguration").withPeriod(duration)
-   es.enable("jdk.Flush").withoutThreshold
+  //  es.onEvent("jdk.JavaMonitorEnter", event => Kamon.gauge("jdk.java-mohitor-enter.monitor-class").withoutTags().update(event.getFloat("monitorClass")))
 
    // register event handlers
-   es.onEvent("jdk.CPULoad", onCPULoad)
-   es.onEvent("jdk.JavaThreadStatistics", onJavaThreadStatistics)
-   es.onEvent("jdk.SafepointBegin", onSafepointBegin)
-   es.onEvent("jdk.SafepointEnd", onSafepointEnd)
-
-   es.onEvent("jdk.JavaMonitorEnter", event => Kamon.gauge("jdk.java-mohitor-enter.monitor-class").withoutTags().update(event.getFloat("monitorClass")))
-
-   // register event handlers
-   es.onEvent("jdk.GarbageCollection", System.out.println)
-   es.onEvent("jdk.CPULoad", System.out.println)
+  //  es.onEvent("jdk.GarbageCollection", System.out.println)
+  //  es.onEvent("jdk.CPULoad", System.out.println)
    es.onEvent("jdk.JVMInformation", System.out.println)
 
    // start and block
    es.start
  }
 
- def onSafepointBegin(event:RecordedEvent) = {
-   safepointBegin.put(event.getValue("safepointId"), event.getEndTime());
- }
+  def onMetadata(metadata: MetadataEvent):Unit = {
+    metadata.getEventTypes.asScala.filterNot(_.isEnabled).foreach(x => println(x.getName))
+  }
 
- def onSafepointEnd(event:RecordedEvent) = {
-   val id = event.getValue("safepointId").asInstanceOf[Long];
-   val begin = safepointBegin.get(id).getOrElse(null);
-
-   if (begin != null) {
-     val nanos = Duration.between(begin, event.getEndTime()).toNanos()
-     safepointBegin.remove(id);
-     Kamon.histogram("safepoint-time").withoutTags().record(nanos)
-    //  println(s"safepoint-time: $nanos")
-   }
- }
-
- def onJavaThreadStatistics(event:RecordedEvent) = {
-  // println(s"Thread-count: ${event.getDouble("activeCount")}")
- }
-
- def onCPULoad(event:RecordedEvent) = {
-   Kamon.gauge("cpu-load.machine-total").withoutTags().update(event.getDouble("machineTotal"))
- }
-
- for(i <- 1 to 10000) { println(i)}
+  def onEvent(event: RecordedEvent):Unit = {
+    event.getEventType.getName match {
+      case "jdk.CPULoad" => CpuHandler.onCPULoad(event)
+      case "jdk.JavaMonitorEnter" => JavaMonitorHandler.onMonitorEnter(event)
+      case "jdk.GarbageCollection" => GCHandler.onGCRun(event)
+      case "jdk.JavaThreadStatistics" => Threads.onJavaThreadStatistics(event)
+      case "jdk.SafepointBegin" => SafePointHandler.onSafepointBegin(event)
+      case "jdk.SafepointEnd" => SafePointHandler.onSafepointEnd(event)
+      case "jdk.ExecutionSample" => println("====>>>> ExecutionSample"+ event)
+      case "jdk.NetworkUtilization" => println("====>>>> jdk.NetworkUtilization"+ event)
+      case other => println(event)
+    }
+  }
 }
 
 
@@ -106,3 +96,19 @@ import java.time.Instant
 
 ////https://github.com/DataDog/dd-trace-java/blob/master/dd-java-agent/agent-profiling/profiling-controller-openjdk/src/main/java/com/datadog/profiling/controller/openjdk/OpenJdkController.java
 //
+
+ //  es.enable("jdk.CPULoad").withPeriod(duration)
+  //  es.enable("jdk.YoungGarbageCollection").withoutThreshold
+  //  es.enable("jdk.OldGarbageCollection").withoutThreshold
+  //  es.enable("jdk.GCHeapSummary").withPeriod(duration)
+  //  es.enable("jdk.PhysicalMemory").withPeriod(duration)
+  //  es.enable("jdk.GCConfiguration").withPeriod(duration)
+  //  es.enable("jdk.SafepointBegin")
+  //  es.enable("jdk.SafepointEnd")
+  //  es.enable("jdk.ObjectAllocationSample").`with`("throttle", "150/s")
+  //  es.enable("jdk.ExecutionSample").withPeriod(Duration.ofMillis(10)).withStackTrace
+  //  es.enable("jdk.JavaThreadStatistics").withPeriod(duration)
+  //  es.enable("jdk.ClassLoadingStatistics").withPeriod(duration)
+  //  es.enable("jdk.Compilation").withoutThreshold
+  //  es.enable("jdk.GCHeapConfiguration").withPeriod(duration)
+  //  es.enable("jdk.Flush").withoutThreshold
---
+++ src/main/scala/Metrics.scala
@@ -0,0 +1,59 @@
+import scala.collection.concurrent.TrieMap
+import java.time.Instant
+import jdk.jfr.consumer.{EventStream, RecordedEvent, RecordingStream}
+import java.time.Duration
+import kamon.Kamon
+
+enum JfrMetrics(name:String) {
+    case SafePoint extends JfrMetrics("")
+    case CPU extends JfrMetrics("")
+}
+
+object SafePointHandler {
+    private val safepointBegin = TrieMap.empty[Long, Instant]
+
+    def onSafepointBegin(event:RecordedEvent) = {
+        safepointBegin.put(event.getValue("safepointId"), event.getEndTime());
+    }
+
+    def onSafepointEnd(event:RecordedEvent) = {
+        val id = event.getValue("safepointId").asInstanceOf[Long];
+        val begin = safepointBegin.get(id).getOrElse(null);
+
+        if (begin != null) {
+            val nanos = Duration.between(begin, event.getEndTime()).toNanos()
+            safepointBegin.remove(id);
+            Kamon.histogram("safepoint-time").withoutTags().record(nanos)
+            println(s"safepoint-time: $nanos")
+        }
+    }
+}
+
+object CpuHandler {
+    
+    def onCPULoad(event:RecordedEvent) = {
+        System.out.println(event)
+        Kamon.gauge("cpu-load.machine-total").withoutTags().update(event.getDouble("machineTotal"))
+    }
+}
+
+object GCHandler {
+    def onGCRun(event:RecordedEvent) = {
+        System.out.println(event)
+    }
+}
+
+object JavaMonitorHandler {
+    def onMonitorEnter(event:RecordedEvent) = {
+        // Kamon.gauge("jdk.java-mohitor-enter.monitor-class").withoutTags().update(event.getFloat("monitorClass"))
+        System.out.println(event)
+    }
+}
+
+object Threads {
+     def onJavaThreadStatistics(event:RecordedEvent) = {
+        println(s"Thread info: ${event}")
+
+        println(s"Thread-count: ${event.getDouble("activeCount")}")
+    }
+}
