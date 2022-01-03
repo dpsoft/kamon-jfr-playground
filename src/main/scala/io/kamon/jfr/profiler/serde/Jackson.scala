1e395ad7811c991687f5a776c765f92d184add50
Diego <diegolparra@gmail.com>
2022-01-03 11:15:01 -0300

    kafka pusher

--- src/main/scala/io/kamon/jfr/profiler/serde/Jackson.scala
+++ src/main/scala/io/kamon/jfr/profiler/serde/Jackson.scala
@@ -3,10 +3,10 @@ package io.kamon.jfr.profiler.serde
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
-object Jackson {
-  private val mapper = new ObjectMapper()
+object Jackson:
+  private val mapper = ObjectMapper()
   mapper.registerModule(DefaultScalaModule)
 
   def toJson(value: Any): String =
     mapper.writeValueAsString(value)
-}
+
